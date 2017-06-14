998800f94ac7e6311aa03923b1ee663a217d4f8f
Naim Khalifa <n.khalifa@gmail.com>
2017-06-14 15:40:28 +0200

    Add favicons and related files

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css" />
     <!-- Blog specific styles -->
     <link rel="stylesheet" href="/css/app.css" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00d1b2">
+    <meta name="theme-color" content="#00d1b2">
 </head>
 <body>
     @include('_partials._navigation_top')
